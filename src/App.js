feeef566429fc12c624ecc8fefe31ee0a97f2a74
Bettycodes <bettygcodes@gmail.com>
2022-03-04 10:18:32 +0300

    added source code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <footer>
+       Coded by Bethel Girma {" "}
+     <a href='https://github.com/Bettycodes/weather-react-project.git' target="_blank" rel="noreferrer"> Source code</a>
+     </footer>
     </div>
   );
 }
