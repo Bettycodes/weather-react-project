f75698bb3ddb935ccc59ef22d86606456e0840f5
Bettycodes <bettygcodes@gmail.com>
2022-03-04 22:18:56 +0300

    added new components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
-     <div className='weather'>
-       <form>
-         <div className='row'>
-           <div className='col-9'>
-         <input 
-         type="search"
-         placeholder='Enter a city..'
-         className="form-control"
-         />
-         </div>
-         <div className='col-3'>
-         <input type="submit" value="search" className='btn btn-primary w-100'/>
-         </div>
-         </div>
-       </form>
+      <div className="container">
+        <App defaultCity="New York" />
 
-     </div>
-     <h1 className='city'>City</h1>
-     <ul className='first'>
-       <li>Friday 10:31</li>
-       <li>description</li>
-     </ul>
-     <div className='row'>
-       <div className='col-6 temp-icon'>
-         <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="partly cloudy"/>
-       -4°C
-       </div>
-       <div className='col-6'>
-         <ul className='second'>
-           <li>Precipitation: 0% </li>
-           <li>Humidity: 43% </li>
-           <li>Wind: 11 km/h</li>
-         </ul>
-       </div>
-     </div>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.delac.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Matt Delac
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-     <footer>
-       Coded by Bethel Girma {" "}
-     <a href='https://github.com/Bettycodes/weather-react-project.git' target="_blank" rel="noreferrer"> Source code</a>
-     </footer>
     </div>
   );
 }
-
-
